1aaec51718167b090c2ff4808b441e959242c0f4
Moshe Pfeffer <moishipfeffer@gmail.com>
2026-06-19 01:39:21 +0300

    message

--- logic2.py
+++ logic2.py
@@ -3,3 +3,7 @@ door_locked = True
 has_key = False
 admin_override = True
 print(admin_override or has_key and door_locked)
+#section 2
+message = "" 
+backup_message = "No message found"
+print(message or backup_message)
