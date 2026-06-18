22e2dca5817c4018e75fd42ad1bc59b3ca837ce0
Moshe Pfeffer <moishipfeffer@gmail.com>
2026-06-19 01:04:57 +0300

    guest or name

--- logic.py
+++ logic.py
@@ -26,3 +26,8 @@ x = None
 y = 42
 print(x or y)
 # the value of y is retured becous y is the true wan
+#section 8
+username = "" 
+default = "guest"
+final_username=default or username
+print(final_username)
