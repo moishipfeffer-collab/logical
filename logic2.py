3abad910b54bf9dd4ee602087892d99b3d3e6ff0
Moshe Pfeffer <moishipfeffer@gmail.com>
2026-06-19 02:03:54 +0300

    pass if score >=60

--- logic2.py
+++ logic2.py
@@ -12,4 +12,11 @@ x = None
 print(x is None)
 #befuor using a value chek if it exists
 #section 4
-
+a = 0
+b = "ready"
+c = 42
+print(a or b or c)
+#the or is giving the first value thats true
+#section 5
+score = 85
+print ("pass") if score>=60 else print("fail")
